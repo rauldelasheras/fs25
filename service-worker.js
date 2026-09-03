cb19abf63965cefd4b79256be306948a81b6ffe3
rauldelasheras <96698488+rauldelasheras@users.noreply.github.com>
2026-09-03 15:58:36 +0200

    Add files via upload

--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,4 @@
-const CACHE = 'fs25-checklist-v322';
+const CACHE = 'fs25-checklist-v33';
 const STATIC_ASSETS = ['./manifest.webmanifest','./icon-180.png','./icon-192.png','./icon-512.png'];
 
 self.addEventListener('install', event => {
